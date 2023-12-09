4ec553fe301ea7104f6b81f2070c49e1ebfc4ba9
TheLTMProject <153396419+TheLTMProject@users.noreply.github.com>
2023-12-09 17:08:19 -0500

    Update AbundanceEstimate.py

--- AbundanceEstimate.py
+++ AbundanceEstimate.py
@@ -120,4 +120,276 @@ with open('IslandsiHab.pkl', 'rb') as f:
 HabSig = np.load("Malaysia_HabSig.npy")
 KSWSHabSig = np.load("KSWSHabSig.npy")
 CTHabSig = np.load("CTHabSig.npy")
-np.shape(HabSig), np.shape(KSWSHabSig), np.shape(CTHabSig), 
+np.shape(HabSig), np.shape(KSWSHabSig), np.shape(CTHabSig)
+
+
+
+'''Associates Signals to Expected Population and Averages spots of equal habitability'''
+Exp_NgivenS = np.load("Exp_NgivenS0.523813.npy")
+KSWSPopValue = 1566
+Exp_NgivenS = np.load("Exp_NgivenS0.0.npy")
+KSWSPopValue = 792
+Exp_NgivenS = np.load("Exp_NgivenS0.999999.npy")
+KSWSPopValue = 3097
+
+HabPop = np.zeros((len(HabSig),2))
+for ihabsig, habsig in enumerate(HabSig):
+    HabPop[ihabsig][:] = [habsig[0],Exp_NgivenS[int(habsig[1])]]
+
+HabSet = np.array(list(set(HabPop[:,0])))
+HabSet = HabSet[np.argsort(HabSet)]
+HabPopSpots = np.zeros((len(HabSet),3))
+
+for ihabset, habset in enumerate(HabSet):
+    spots = np.where(HabPop[:,0] == habset)[0]
+    pop = np.mean(HabPop[spots],axis=0)[1]
+    HabPopSpots[ihabset] = [habset,pop,len(spots)]
+
+np.save("HabPopSpots.npy",HabPopSpots)
+HabPopSpots = np.load("HabPopSpots.npy")
+
+
+'''Scans and saves all possible thresholds for mean-filter'''
+matplotlib.use('Agg')
+
+iDiffusion = np.argmin(np.abs(HabPopSpots[:, 0, None] - ColGrad[:, -1][::-1][1:]), axis=1)
+ThresholdMaximum = lenColGrad-1
+SmoothingFactor = 0.1
+MegaMeanZipSpline = np.zeros((lenColGrad,lenColGrad))
+for sanity in range(2,lenColGrad):
+    PixelCount = np.load("MegaMeanFilterPxCount.npy")[sanity] #Using the pixel count for a sanity MeanFieldFilter
+    PixelDiffusion = PixelCount[iDiffusion] #Obtains the diffusion for all habitabilities in HabPopSpots
+
+    Diffused_HabPop = np.zeros((len(HabPopSpots),2))
+    for ihabpopspot, habpopspot in enumerate(HabPopSpots): #For every signal in HabPopSpots
+        Diffused_HabPop[ihabpopspot] = habpopspot[0], habpopspot[1]*(habpopspot[2]/PixelDiffusion[ihabpopspot])
+            #Calculates the diffused populaiton as = (mean population_h)*(number of signal spots_h)
+                                                                    # /(MeanFiltered number of spots_h)
+
+    '''Applies MeanFieldFilter for the LTMs per Pixel Spot according to Habitability given relation above'''
+    X,Y = Diffused_HabPop[:,0],Diffused_HabPop[:,1] 
+    MegaMeanFilter_DiffusedHabPop = np.zeros((ThresholdMaximum+1, len(iDiffusion)))
+    '''Applies MeanFilter of given Thresholds to XY relation'''
+    for threshold in range(ThresholdMaximum+1):
+        MeanFieldThreshold, MeanFilterY = (threshold/(lenColGrad-1)), []
+        
+        for x,y in zip(X,Y):
+            MeanFilterY.append(np.mean(Y[(np.abs(X - x) <= MeanFieldThreshold)]))
+        MegaMeanFilter_DiffusedHabPop[threshold] = MeanFilterY
+
+    '''Using MeanFilters above, constructs associated splines for entire ColGrad'''
+    MegaSplines = np.zeros((ThresholdMaximum+1,2,lenColGrad-1))
+    for threshold in range(ThresholdMaximum+1):
+        USpline = UnivariateSpline(Diffused_HabPop[:,0],MegaMeanFilter_DiffusedHabPop[threshold],
+                                s = ThresholdMaximum/(SmoothingFactor*lenColGrad*threshold) if threshold != 0 else 5, 
+                                k=2, ext=0)
+        MegaSplines[threshold] = ColGrad[:,-1][::-1][1:], USpline(ColGrad[:,-1][::-1][1:])
+
+
+    '''Using KSWSMap, identifies the IDFactos for each Spline and obtains its Mean'''
+    IDFactors = KSWSPopValue / np.sum(MegaSplines[:,-1,KSWSiHab],axis=1)
+    MeanZipSpline = np.mean([(IDfactor*Spline[-1,:]) for IDfactor, Spline in zip(IDFactors,MegaSplines)],axis=1)
+    MegaMeanZipSpline[sanity] = MeanZipSpline
+    
+    fig, ax = plt.subplots(figsize=(9,4))
+    ax.tick_params(direction='in')
+    ax.set_xlabel("Habitat Preference",fontsize=1.25*fs)
+    ax.set_ylabel("Expected Number of LTMs per Spot",fontsize=1.25*fs)
+    plt.title(f"Threshold = {np.round(100*sanity/(lenColGrad-1),2)}%",fontsize=1.25*fs)
+
+    plt.plot(ColGrad[:,-1][::-1], MeanZipSpline, 'g', linewidth = 0.1*fs)
+    plt.show()
+
+    plt.savefig(f"C:\\...\\wdir\\wdirPlots\\Threshold = {sanity}.png", dpi=300, bbox_inches='tight')
+    plt.close()
+    clear_output()
+    
+    '''Using Mean Spline, prints populations for Map and KSWS'''
+    print("\nMap", np.sum(MeanZipSpline[MapiHab]) )
+    print("KSWS", np.sum(MeanZipSpline[KSWSiHab]),)
+    print("CT", np.sum(MeanZipSpline[CTiHab]),"\n")
+    FinalSum = 0
+    for icountry, country in enumerate(Countries):
+            countryPopulation = np.sum(MeanZipSpline[CountriesiHab[icountry]])
+            print(country, countryPopulation)
+            FinalSum += countryPopulation 
+    print("\nCountries Sum", FinalSum)
+    print(100*sanity/lenColGrad)
+
+np.save("MegaMeanZipSpline0.523813.npy", MegaMeanZipSpline)
+np.save("MegaMeanZipSpline0.0.npy", MegaMeanZipSpline)
+np.save("MegaMeanZipSpline0.999999.npy", MegaMeanZipSpline)
+
+
+'''Uses average of scanning thresholds to obtain estimate wildlife abundance'''
+fig, ax = plt.subplots(figsize=large)
+ax.tick_params(direction='in')
+ax.set_xlabel("Habitat Preference",fontsize=1.25*fs)
+ax.set_ylabel("Expected LTMs per Spot (individuals)",fontsize=1.25*fs)
+plt.title(f"LTM Population according to Habitat Preference",fontsize=1.25*fs)
+
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.999999.npy")
+MeanMeanZipSpline = np.mean(MegaMeanZipSpline[1:], axis=0)
+plt.plot(ColGrad[1:,-1][::-1], MeanMeanZipSpline[1:], 'r', linewidth = 0.2*fs, label = "Upper Bound, $ρ_{upper}$")
+
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.523813.npy")
+MeanMeanZipSpline = np.mean(MegaMeanZipSpline[1:], axis=0)
+plt.plot(ColGrad[1:,-1][::-1], MeanMeanZipSpline[1:], 'g', linewidth = 0.2*fs, label = "Best Estimation, $ρ_{sanctuary}$")
+
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.0.npy")
+MeanMeanZipSpline = np.mean(MegaMeanZipSpline[1:], axis=0)
+plt.plot(ColGrad[1:,-1][::-1], MeanMeanZipSpline[1:], 'b', linewidth = 0.2*fs, label = "Lower Bound, $ρ_{lower}$")
+
+plt.legend()
+
+plt.savefig(f"C:\\...\\wdir\\Malaysia_MeanSplines.png", dpi=600, bbox_inches='tight')
+
+plt.show()
+
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.0.npy")
+MeanMeanZipSpline = np.mean(MegaMeanZipSpline[1:], axis=0)
+a = np.max(MeanMeanZipSpline) / 1.951 
+print(a)
+print(a*1.952)
+
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.523813.npy")
+MeanMeanZipSpline = np.mean(MegaMeanZipSpline[1:], axis=0)
+b= np.max(MeanMeanZipSpline) / 1.951 
+print(b)
+print(b*1.952)
+
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.999999.npy")
+MeanMeanZipSpline = np.mean(MegaMeanZipSpline[1:], axis=0)
+
+c = np.max(MeanMeanZipSpline) / 1.951 
+
+print(c)
+print(c*1.952)
+
+
+'''Computes abudance per region'''
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.523813.npy")
+A = np.mean(MegaMeanZipSpline[1:], axis=0)
+
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.999999.npy")
+B = np.mean(MegaMeanZipSpline[1:], axis=0)
+
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.0.npy")
+C = np.mean(MegaMeanZipSpline[1:], axis=0)
+
+
+
+Populations =[]
+
+
+for j in range(0,3):
+        Populations.append([])
+        for i in [A,B,C][j:j+1]:
+                MeanZipSpline = i
+                print("\nMap", np.sum(MeanZipSpline[MapiHab]) )
+                print("KSWS", np.sum(MeanZipSpline[KSWSiHab]),)
+                print("CT", np.sum(MeanZipSpline[CTiHab]),"\n")
+                
+                FinalSum = 0
+                
+                for icountry, country in enumerate(Countries):
+                        countryPopulation = np.sum(MeanZipSpline[CountriesiHab[icountry]])
+                        print(country, int(np.round(countryPopulation)), int(np.round(countryPopulation**0.5)))
+                        Populations[j].append(countryPopulation)
+                        FinalSum += countryPopulation
+
+                for iisland, island in enumerate(Islands):
+                        islandPopulation = np.sum(MeanZipSpline[IslandsiHab[iisland]])
+                        print(island, int(np.round(islandPopulation)), int(np.round(islandPopulation**0.5)))
+                        Populations[j].append(islandPopulation)
+                        FinalSum += islandPopulation
+
+                print("\nCountries Sum", FinalSum,"\n\n\n")
+
+# Create a vertical bar plot
+LowerPopulation, OptimalPopulation, UpperPopulation = Populations[2], Populations[0], Populations[1]
+
+LowerPlot = np.array([x/1e3 for x in LowerPopulation])
+OptimalPlot = np.array([x/1e3 for x in OptimalPopulation])
+UpperPlot = np.array([x/1e3 for x in UpperPopulation])
+Countries = np.array(Countries)
+iCountriesFocus = [0,2,3,-3]
+
+plt.figure(figsize=large)
+plt.bar(Countries[iCountriesFocus], LowerPlot[iCountriesFocus], label="Lower Bound, $ρ_{lower}$", color='b')
+plt.bar(Countries[iCountriesFocus], OptimalPlot[iCountriesFocus] , label="Best Estimation, $ρ_{sanctuary}$", bottom=LowerPlot[iCountriesFocus], color='g')
+plt.bar(Countries[iCountriesFocus], UpperPlot[iCountriesFocus], label="Upper Bound, $ρ_{upper}$", bottom=[LowerPlot[i] + OptimalPlot[i] for i in iCountriesFocus], color='r')
+
+plt.xlabel('Countries',fontsize=1.25*fs)
+plt.ylabel('$M.$ $fascicularis$ Abundance (thousands)',fontsize=1.25*fs)
+plt.title('Abundance Estimation of $M.$ $fascicularis$ across High Confidence Countries',fontsize=1.25*fs)
+plt.legend()
+plt.show()
+
+
+iCountriesFocus = [1,4,5,7]
+
+plt.figure(figsize=large)
+plt.bar(Countries[iCountriesFocus], LowerPlot[iCountriesFocus], label="Lower Bound, $ρ_{lower}$", color='b')
+plt.bar(Countries[iCountriesFocus], OptimalPlot[iCountriesFocus] , label="Best Estimation, $ρ_{sanctuary}$", bottom=LowerPlot[iCountriesFocus], color='g')
+plt.bar(Countries[iCountriesFocus], UpperPlot[iCountriesFocus], label="Upper Bound, $ρ_{upper}$", bottom=[LowerPlot[i] + OptimalPlot[i] for i in iCountriesFocus], color='r')
+
+plt.xlabel('Countries',fontsize=1.25*fs)
+plt.ylabel('$M.$ $fascicularis$ Abundance (thousands)',fontsize=1.25*fs)
+plt.title('Abundance Estimation of $M.$ $fascicularis$ across Lower Confidence Countries',fontsize=1.25*fs)
+plt.legend()
+plt.show()
+
+
+
+'''Plots total result curves'''
+fig, ax = plt.subplots(figsize=(9,4))
+ax.tick_params(direction='in')
+ax.set_xlabel("Habitat Preference",fontsize=1.25*fs)
+ax.set_ylabel("Expected Number of LTMs per Spot\n(individuals)",fontsize=1.25*fs)
+plt.title(f"LTM Population according to Habitat Preference",fontsize=1.25*fs)
+
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.0.npy")
+MeanMeanZipSpline = np.mean(MegaMeanZipSpline[1:], axis=0)
+plt.plot(ColGrad[1:,-1][::-1], MeanMeanZipSpline[1:], 'b', linewidth = 0.2*fs, label = "Lower Bound, $ρ_{lower}$")
+
+plt.legend()
+
+plt.savefig(f"C:\\...\\Plots\\Lower.png", dpi=600, bbox_inches='tight')
+
+plt.show()
+np.mean(MeanMeanZipSpline)
+
+fig, ax = plt.subplots(figsize=(9,4))
+ax.tick_params(direction='in')
+ax.set_xlabel("Habitat Preference",fontsize=1.25*fs)
+ax.set_ylabel("Expected Number of LTMs per Spot\n(individuals)",fontsize=1.25*fs)
+plt.title(f"LTM Population according to Habitat Preference",fontsize=1.25*fs)
+
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.523813.npy")
+MeanMeanZipSpline = np.mean(MegaMeanZipSpline[1:], axis=0)
+plt.plot(ColGrad[1:,-1][::-1], MeanMeanZipSpline[1:], 'g', linewidth = 0.2*fs, label = "Best Estimation, $ρ_{opt}$")
+
+plt.legend()
+
+plt.savefig(f"C:\\...\\Plots\\Best.png", dpi=600, bbox_inches='tight')
+
+plt.show()
+np.mean(MeanMeanZipSpline)
+
+fig, ax = plt.subplots(figsize=(9,4))
+ax.tick_params(direction='in')
+ax.set_xlabel("Habitat Preference",fontsize=1.25*fs)
+ax.set_ylabel("Expected Number of LTMs per Spot\n(individuals)",fontsize=1.25*fs)
+plt.title(f"LTM Population according to Habitat Preference",fontsize=1.25*fs)
+
+MegaMeanZipSpline = np.load("MegaMeanZipSpline0.999999.npy")
+MeanMeanZipSpline = np.mean(MegaMeanZipSpline[1:], axis=0)
+plt.plot(ColGrad[1:,-1][::-1], MeanMeanZipSpline[1:], 'r', linewidth = 0.2*fs, label = "Upper Bound, $ρ_{upper}$")
+
+plt.legend()
+
+plt.savefig(f"C:\\...\\Plots\\Upper.png", dpi=600, bbox_inches='tight')
+
+plt.show()
+np.mean(MeanMeanZipSpline)
